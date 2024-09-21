fa6a4c4338a403253001f7051732f1f5ab579647
albeany <atabernerovalle@gmail.com>
2024-09-22 01:40:26 +0200

    en casa el finde registro 1 state

--- src/componentes/zonaCliente/registroComponent/Registro.js
+++ src/componentes/zonaCliente/registroComponent/Registro.js
@@ -3,135 +3,118 @@ import { useState } from "react"; //hook para manejar el estado del componente
 
 function Registro() {
   //1º props componente
+  let formOk = false;
+  //console.log
+
   //let { username, password }=props;
 
   //2º state componente
-  let [campoCuenta, setCampoCuenta] = useState({
-    nombre: "",
-    apellidos: "",
-    email: "",
-    password: "",
-    formularioValido: false,
-    esValido: {
-      nombre: false,
-      apellidos: false,
-      email: false,
-      password: false,
+  //TODO ... voy a crear en el state del componente un UNICO OBJETO para recoger y validar todos los campos del formulario:
+  //!     - este objeto tiene como propiedades los CAMPOS DEL FORMULARIO: nombre, apellidos, email, password
+  //!     - dentro de cada propiedad voy a meter un objeto q represente valores del campo del input del formulario:
+  //!             - valor caja de texto, validaciones, mensaje de error a mostrar, estado de validacion
+  const [formData, setFormData] = useState({
+    nombre: {
+      //? <---------- propiedad q se mapea contra campo input-nombre
+      valor: "", // <------ propiedad de "nombre" a modificar en evento onChange del input
+      valido: false, // <--propiedad de "nombre" q define estado de validacion del contenido del input-nombre
+      validaciones: {
+        // <-- propiedad de "nombre" con las validaciones a hacer sobre el input-nombre
+        obligatorio: [true, "* Nombre obligatorio"],
+        maximaLongitud: [150, "* Nombre no debe exceder de 150 cars."],
+        patron: [
+          /^[A-Z][a-z]+(\s[A-Z][a-z]+)*$/,
+          "* Formato invalido nombre, ej: Nuria Roca",
+        ],
+      },
+      mensajeValidacion: "", //<-- propiedad de "nombre" con el mensaje de error procedente de las validaciones a hacer sobre input-nombre
     },
-    mensajeError: { nombre: "", apellidos: "", email: "", password: "" }
+    apellidos: {
+      //? <---------- propiedad q se mapea contra campo input-apellidos
+      valor: "",
+      valido: false,
+      validaciones: {
+        obligatorio: [true, "* Apellidos obligatorios"],
+        maximaLongitud: [250, "* Apellidos no debe exceder de 250 cars."],
+        patron: [
+          /^[A-Z][a-z]+(\s[A-Z][a-z]+)*$/,
+          "* Formato invalido apellidos, ej: Nuria Roca",
+        ],
+      },
+      mensajeValidacion: "",
+    },
+    email: {
+      //? <---------- propiedad q se mapea contra campo input-email
+      valor: "",
+      valido: false,
+      validaciones: {
+        obligatorio: [true, "* Email obligatorios"],
+        patron: [
+          /^.+@(hotmail|gmail|yahoo|msn)\.[a-z]{2,3}$/i,
+          "* Formato invalido email, ej: mio@hotmail.es",
+        ],
+      },
+      mensajeValidacion: "",
+    },
+    password: {
+      //? <---------- propiedad q se mapea contra campo input-password
+      valor: "",
+      valido: false,
+      validaciones: {
+        obligatorio: [true, "* Pasword obligatorio"],
+        patron: [
+          /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$!%*?&])[A-Za-z\d@#$!%*?&]{8,}$/,
+          "* Formato invalido Debe tener al menos 8 caracteres, una mayúscula, minúscula, número y un símbolo @#$!%*?&",
+        ],
+      },
+      mensajeValidacion: "",
+    },
   });
 
   //3º codigo funcional javascript (hooks, tratamiento eventos, ...)
 
-//#region VALIDAR CAMPOS
-  //TODO validaciones:
-  //! ----- nombre: rellenado, max.longitud de 150 caracteres, solo admite mays o mins y espacios
-  //! ----- apellidos: rellenado, max.longitud de 250 caracteres, solo adminte mays o mins y espacios
-  //! ----- email: formato email, dominios admitidos: gmail,hotmail,msn,yahoo
-  //! ----- password: min 8 caracteres, una MAYS, una MINS, un digito y un simbolo raro
-
-  function validaciones(ev) {
-    //console.log("id es ", ev.target.id);
-    //console.log("valor es ", ev.target.value);
-
-    let valor = ev.target.value;
-    let campo = ev.target.id; //id cambiado de txtNombre a nombre por comodidad para usar como parametro...
-    let valido = false;
-    let msgError = "";
-    let todoValido = false;
-
-    const regex = {
-      nombre: /^[A-Z][a-z]*(\s[A-Z][a-z]*)*$/,
-      apellidos: /^[A-Z][a-z]*(\s[A-Z][a-z]*)*$/,
-      email: /^[A-Za-z].*@(gmail|hotmail|msn|yahoo)\..*/i,
-      password:
-        /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$!%*?&])[A-Za-z\d@#$!%*?&]{8,}$/,
-    };
-
-    if (!valor) {
-      msgError = "Campo obligatorio";
-      actualizarState(campo, valor, valido, msgError, todoValido);
-      return;
-    }
-    if (valor.length > 150) {
-      msgError = "Máximo 150 caracteres";
-      actualizarState(campo, valor, valido, msgError, todoValido);
-      return;
-    }
-
-    switch (campo) {
-      case "nombre":
-        if (!regex[campo].test(valor)) {
-          // [campo] es String como literal, pero por sintaxis no se pone regex.[campo]
-          msgError = 'Formato incorrecto, ejem: "Maria Luisa"';
-        } else {
-          msgError = "";
-          valido = true;
-        }
-        break;
-
-      case "apellidos":
-        if (!regex[campo].test(valor)) {
-          msgError = 'Formato incorrecto, ejem: "Perez Gomez"';
-        } else {
-          msgError = "";
-          valido = true;
-        }
-        break;
-      case "email":
-        if (!regex[campo].test(valor)) {
-          msgError = "Sólo se admiten correos de gmail, hotmail, msn y yahoo";
-        } else {
-          msgError = "";
-          valido = true;
-        }
-        break;
-      case "password":
-        if (!regex[campo].test(valor)) {
-          console.log(valor);
-          
-          msgError =
-            "Debe tener al menos 8 caracteres, una mayúscula, minúscula, número y un símbolo @#$!%*?&";
-        } else {
-          msgError = "";
-          valido = true;
-        }
-        break;
-      default:
-        break;
-    }
-    todoValido = campoCuenta.esValido.nombre && campoCuenta.esValido.apellidos && campoCuenta.esValido.email && campoCuenta.esValido.password;
-    console.log('Todos los campos son: ',todoValido);
-    
-    actualizarState(campo, valor, valido, msgError, todoValido);
-  }
-//#endregion VALIDAR CAMPOS
-
-//#region ACTUALIZAR STATE
-  function actualizarState(campo, valor, valido, msgError, todoValido) {
-    setCampoCuenta((prevState) => ({
-      // es aconsejable usar prevState al actulizar states por estabilidad
-      ...prevState,
-      [campo]: valor,
-      formularioValido: todoValido,
-      esValido: { ...prevState.esValido, [campo]: valido }, // valido true o false. campo es nombre o apellido o ....
-      mensajeError: { ...prevState.mensajeError, [campo]: msgError },
-    }));
-  }
-//#endregion ACTUALIZAR STATE
-
   function ManejarSubmitForm(ev) {
     ev.preventDefault(); //anula el comportamiento por defecto del submitform, envio de datos...
-    // ----------antes de definir state ------------------------
-    // cuenta.nombre=document.getElementById('nombre').value;
-    // cuenta.apellidos=document.getElementById('apellidos').value;
-    // cuenta.email=document.getElementById('email').value;
-    // cuenta.password=document.getElementById('password').value;
-    // let cuenta={ nombre,apellidos,email,password };
-    // console.log('datos de cuenta a registrar...', cuenta);
   }
 
-  //4º codigo JSX
+  function validaCajasHandler(ev) {
+    const campo = ev.target.name;
+    const valorCampo = ev.target.value;
+    console.log("Has perdido el foco de... ", campo, valorCampo);
+    const validacionesHacer = formData[campo].validaciones;
+    let msj = "";
+    let _valido = false;
+
+    if (validacionesHacer.obligatorio[0] && !valorCampo) {
+      msj = validacionesHacer.obligatorio[1];
+    } else if (
+      validacionesHacer.maximaLongitud &&
+      valorCampo.length > validacionesHacer.maximaLongitud[0]
+    ) {
+      msj = validacionesHacer.maximaLongitud[1];
+    } else if (!validacionesHacer.patron[0].test(valorCampo)) {
+      msj = validacionesHacer.patron[1];
+    } else _valido = true;
+
+    console.log(msj);
+
+    // Actualizo state para msj
+    setFormData({
+      ...formData,
+      [campo]: { ...formData[campo], mensajeValidacion: msj, valido: _valido },
+    });
+
+    // comprubo si el formulario es valido
+
+    console.log(_valido);
+    console.log(
+      formData.nombre.valido,
+      formData.apellidos.valido,
+      formData.email.valido,
+      formData.password.valido
+    );
+  }
+
   return (
     <div className="container">
       {/* fila donde va logo de ebay y link para el Login*/}
@@ -165,45 +148,50 @@ function Registro() {
               <div className="col form-floating">
                 <input
                   type="text"
-                  id="nombre"
+                  id="txtNombre"
+                  name="nombre"
                   className="form-control form-element"
                   placeholder="Nombre"
-                  onChange={(ev) => {
-                    setCampoCuenta({ ...campoCuenta, nombre: ev.target.value });
-                  }}
-                  onBlur={validaciones}
+                  onChange={(ev) =>
+                    setFormData({
+                      ...formData,
+                      nombre: { ...formData.nombre, valor: ev.target.value }
+                    })
+                  }
+                  onBlur={validaCajasHandler}
                 />
-                {!campoCuenta.esValido.nombre && (
-                  <span className="text-danger">
-                    {" "}
-                    {campoCuenta.mensajeError.nombre}{" "}
-                  </span>
-                )}
-                <label htmlFor="nombre" className="floating-label">
+                <span className="text-danger">
+                  {" "}
+                  {formData.nombre.mensajeValidacion}{" "}
+                </span>
+                <label htmlFor="txtNombre" className="floating-label">
                   Nombre
                 </label>
               </div>
               <div className="col mb-3 form-floating">
                 <input
                   type="text"
-                  id="apellidos"
+                  id="txtApellidos"
+                  name="apellidos"
                   className="form-control form-element"
                   placeholder="Apellidos"
                   onChange={(ev) =>
-                    setCampoCuenta({
-                      ...campoCuenta,
-                      apellidos: ev.target.value,
+                    setFormData({
+                      ...formData,
+                      apellidos: {
+                        ...formData.apellidos,
+                        valor: ev.target.value,
+                      },
                     })
                   }
-                  onBlur={validaciones}
+                  onBlur={validaCajasHandler}
                 />
-                {!campoCuenta.esValido.apellidos && (
-                  <span className="text-danger">
-                    {" "}
-                    {campoCuenta.mensajeError.apellidos}{" "}
-                  </span>
-                )}
-                <label htmlFor="apellidos" className="floating-label">
+                <span className="text-danger">
+                  {" "}
+                  {formData.apellidos.mensajeValidacion}{" "}
+                </span>
+
+                <label htmlFor="txtApellidos" className="floating-label">
                   Apellidos
                 </label>
               </div>
@@ -211,48 +199,47 @@ function Registro() {
             <div className="mb-3 form-floating">
               <input
                 type="email"
-                id="email"
+                id="txtEmail"
+                name="email"
                 className="form-control form-element"
                 placeholder="Correo electrónico"
                 onChange={(ev) =>
-                  setCampoCuenta({
-                    ...campoCuenta,
-                    email: ev.target.value,
+                  setFormData({
+                    ...formData,
+                    email: { ...formData.email, valor: ev.target.value },
                   })
                 }
-                onBlur={validaciones}
+                onBlur={validaCajasHandler}
               />
-              {!campoCuenta.esValido.email && (
-                  <span className="text-danger">
-                    {" "}
-                    {campoCuenta.mensajeError.email}{" "}
-                  </span>
-                )}
-              <label htmlFor="email" className="floating-label">
+              <span className="text-danger">
+                {" "}
+                {formData.email.mensajeValidacion}{" "}
+              </span>
+
+              <label htmlFor="txtEmail" className="floating-label">
                 Correo Electronico
               </label>
             </div>
             <div className="mb-3 form-floating">
               <input
                 type="password"
-                id="password"
+                id="txtPassword"
+                name="password"
                 className="form-control form-element"
                 placeholder="Contraseña"
                 onChange={(ev) =>
-                  setCampoCuenta({
-                    ...campoCuenta,
-                    password: ev.target.value,
+                  setFormData({
+                    ...formData,
+                    password: { ...formData.password, valor: ev.target.value },
                   })
                 }
-                onBlur={validaciones}
+                onBlur={validaCajasHandler}
               />
-              {!campoCuenta.esValido.password && (
-                  <span className="text-danger">
-                    {" "}
-                    {campoCuenta.mensajeError.password}{" "}
-                  </span>
-                )}
-              <label htmlFor="password" className="floating-label">
+              <span className="text-danger">
+                {" "}
+                {formData.password.mensajeValidacion}{" "}
+              </span>
+              <label htmlFor="txtPassword" className="floating-label">
                 Contraseña
               </label>
             </div>
@@ -278,7 +265,14 @@ function Registro() {
             <button
               type="submit"
               className="btn w-100 mb-3"
-              disabled={campoCuenta.formularioValido ? false : true}
+              disabled={
+                !(
+                  formData.nombre.valido &&
+                  formData.apellidos.valido &&
+                  formData.email.valido &&
+                  formData.password.valido
+                )
+              }
             >
               Crear cuenta personal
             </button>
@@ -310,5 +304,4 @@ function Registro() {
     </div>
   );
 }
-
 export default Registro;
